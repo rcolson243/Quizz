5e124b852d3234f65e6084be1dc9bf34bfdac666
rapahel colson <rapahael.kasese@gmailcom>
2021-01-18 23:33:34 +0100

    début du code js

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const form = document.querySelectorAll('.form-quizz');
+const form = document.querySelector('.form-quizz');
 let tableauResults = [];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    for(let i = 1; i < 6; i++){
+       // tableauResults.push(‘‘) je me suis arrêter ici
+    }
+})
